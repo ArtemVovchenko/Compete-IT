63c64dbeef7eeca53801e756975bd73718157257
Artem <artem.vovchenko@nure.ua>
2020-05-30 18:05:58 +0300

    Added Setters to the Competition Class

--- parsers/competition.py
+++ parsers/competition.py
@@ -1,16 +1,14 @@
 class Competition:
 
-    title = None
-    location = None
-    start_date = None
-    end_date = None
-    description = None
-    type = None
-    image = None
-    link = None
-
     def __init__(self):
-        pass
+        self.title = None
+        self.location = None
+        self.start_date = None
+        self.end_date = None
+        self.description = None
+        self.type = None
+        self.image = None
+        self.link = None
 
     def set_title(self, title: str):
         self.title = title
@@ -23,3 +21,18 @@ class Competition:
     def set_start_date(self, start_date: str):
         self.start_date = start_date
         return self
+
+    def set_description(self, description: str):
+        self.description = description
+
+    def set_end_date(self, end_date: str):
+        self.end_date = end_date
+
+    def set_type(self, competition_type: str):
+        self.type = competition_type
+
+    def set_image(self, image: object):
+        self.image = image
+
+    def __str__(self):
+        return f"{self.title} , {self.type} , {self.link}"
