756e27cdc5e32229921d3f50324fbe345bf93a47
Artem <artem.vovchenko@nure.ua>
2020-06-01 08:28:20 +0300

    Initial Commit init_parser.py

--- parsers/init_parser.py
+++ parsers/init_parser.py
@@ -1,1 +1,6 @@
+from parsers.competition import Competition
+from parsers import parser_module
+from parsers import parser_support_module
+
+competitions = parser_support_module.parse() + parser_module.parse()
 
