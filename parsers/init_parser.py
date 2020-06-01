af72d6ef02b3d9c215222ce09afe1b54348fcb7a
Artem <artem.vovchenko@nure.ua>
2020-06-01 08:45:49 +0300

    Initial Commit init_parser.py

--- parsers/init_parser.py
+++ parsers/init_parser.py
@@ -1,6 +1,9 @@
-from parsers.competition import Competition
 from parsers import parser_module
 from parsers import parser_support_module
 
+
 competitions = parser_support_module.parse() + parser_module.parse()
+with open('parsed_rocket.txt','w') as outfile:
+    for competition_data in competitions:
+        outfile.write(str(competition_data))
 
